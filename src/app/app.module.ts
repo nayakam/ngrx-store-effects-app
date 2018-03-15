49d8ecb0a26e16094de556f76e36186ebe4335b0
nayakam <thillakan@gmail.com>
2018-03-16 05:49:49 +1100

    Step 7: Custom Router State Serializer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,18 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { Routes, RouterModule } from '@angular/router';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {RouterModule, Routes} from '@angular/router';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
-import { StoreModule, MetaReducer } from '@ngrx/store';
-import { EffectsModule } from '@ngrx/effects';
+import {MetaReducer, StoreModule} from '@ngrx/store';
+import {EffectsModule} from '@ngrx/effects';
 
-import {reducers} from "./store";
-
+import {CustomSerializer, reducers} from "./store";
 // not used in production
-import { StoreDevtoolsModule } from '@ngrx/store-devtools';
-import { storeFreeze } from 'ngrx-store-freeze';
+import {StoreDevtoolsModule} from '@ngrx/store-devtools';
+import {storeFreeze} from 'ngrx-store-freeze';
+// bootstrap
+import {AppComponent} from './containers/app/app.component';
+import {RouterStateSerializer, StoreRouterConnectingModule} from "@ngrx/router-store";
 
 // this would be done dynamically with webpack for builds
 const environment = {
@@ -22,12 +24,9 @@ export const metaReducers: MetaReducer<any>[] = !environment.production
   ? [storeFreeze]
   : [];
 
-// bootstrap
-import { AppComponent } from './containers/app/app.component';
-
 // routes
 export const ROUTES: Routes = [
-  { path: '', pathMatch: 'full', redirectTo: 'products' },
+  {path: '', pathMatch: 'full', redirectTo: 'products'},
   {
     path: 'products',
     loadChildren: '../products/products.module#ProductsModule',
@@ -39,11 +38,15 @@ export const ROUTES: Routes = [
     BrowserModule,
     BrowserAnimationsModule,
     RouterModule.forRoot(ROUTES),
-    StoreModule.forRoot(reducers, { metaReducers }),
+    StoreModule.forRoot(reducers, {metaReducers}),
     EffectsModule.forRoot([]),
+    StoreRouterConnectingModule,
+
     environment.development ? StoreDevtoolsModule.instrument() : [],
   ],
+  providers: [{provide: RouterStateSerializer, useClass: CustomSerializer}],
   declarations: [AppComponent],
   bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule {
+}
--- src/app/store/reducers/index.ts
+++ src/app/store/reducers/index.ts
@@ -1,5 +1,5 @@
 import *  as fromRouter from '@ngrx/router-store';
-import {Params} from "@angular/router";
+import {ActivatedRouteSnapshot, Params, RouterStateSnapshot} from "@angular/router";
 import {ActionReducerMap, createFeatureSelector} from "@ngrx/store";
 
 export interface RouterStateUrl {
@@ -18,3 +18,21 @@ export const reducers: ActionReducerMap<State> = {
 
 //selector
 export const getRouterState = createFeatureSelector<fromRouter.RouterReducerState<RouterStateUrl>>('routerReducer');
+
+//custom serializer
+export class CustomSerializer implements fromRouter.RouterStateSerializer<RouterStateUrl> {
+  serialize(routerState: RouterStateSnapshot): RouterStateUrl {
+    //destructuring the url same as  let url: string = routerState.url
+    const {url} = routerState;
+    const {queryParams} = routerState.root;
+    let state: ActivatedRouteSnapshot = routerState.root;
+    while (state.firstChild) {
+      console.log(state.url, state.params);
+      state = state.firstChild;
+    }
+    const {params} = state;
+    console.log(url, queryParams, params);
+    return {url, queryParams, params} as RouterStateUrl;
+  }
+
+}
