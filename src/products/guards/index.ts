690bf86fd3ede7b0530527a81e7b7b9d397906b3
nayakam <thillakan@gmail.com>
2018-03-26 07:43:53 +1100

    Step 21: Pizza exists guard Step 22: Toppings Guard

--- src/products/guards/index.ts
+++ src/products/guards/index.ts
@@ -1,5 +1,10 @@
 import { PizzasGuard } from './pizzas.guard';
+import { PizzaExistsGuards } from './pizza-exists.guard';
+import { ToppingsGuard } from './toppings.guard';
 
-export const guards: any[] = [PizzasGuard];
+
+export const guards: any[] = [PizzasGuard, PizzaExistsGuards, ToppingsGuard];
 
 export * from './pizzas.guard';
+export * from './toppings.guard';
+export * from './pizza-exists.guard';
